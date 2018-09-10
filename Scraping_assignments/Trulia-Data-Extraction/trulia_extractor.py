940b458474806f5dc0dcd1a1c16b62565dda36e5
Abhisek Roy <abhisekroy1994@gmail.com>
2018-09-11 00:15:40 +0530

    Added a data scraper for Trulia, which is a website that lists homes for sale online throughout the world.

--- Scraping_assignments/Trulia-Data-Extraction/trulia_extractor.py
+++ Scraping_assignments/Trulia-Data-Extraction/trulia_extractor.py
@@ -6,6 +6,8 @@ import urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import json
+import ast
+import os
 from urllib.request import Request, urlopen
 # For ignoring SSL certificate errors
 
@@ -24,8 +26,42 @@ soup = BeautifulSoup(webpage, 'html.parser')
 html = soup.prettify('utf-8')
 product_json = {}
 
+for meta in soup.findAll('meta', attrs={'name': 'description'}):
+    try:
+        product_json['description'] = meta['content']
+        break
+    except:
+        pass
+
+for link in soup.findAll('link', attrs={'rel': 'canonical'}):
+    try:
+        product_json['link'] = link['href']
+        break
+    except:
+        pass
+for scripts in soup.findAll('script', attrs={'type': 'application/ld+json'}):
+    details_json = ast.literal_eval(scripts.text.strip())
+product_json["price"]={}
+product_json["price"]["amount"]=details_json["offers"]["price"]
+product_json["price"]["currency"]=details_json["offers"]["priceCurrency"]
+
+for paragraph in soup.findAll('p', attrs={'id': 'propertyDescription'}):
+    product_json["broad-description"] = paragraph.text.strip()
+product_json["overview"]=[]
+for divs in soup.findAll('div', attrs={'data-auto-test-id':'home-details-overview'}):
+    for divs_second in divs.findAll('div'):
+        for uls in divs_second.findAll('ul'):
+            for lis in uls.findAll('li', text=True, recursive=False):
+                product_json["overview"].append(lis.text.strip())
+
+
+
+with open('house_details.json', 'w') as outfile:
+    json.dump(product_json, outfile, indent=4)
+
 
 with open('output_file.html', 'wb') as file:
     file.write(html)
+
 
 print ('----------Extraction of data is complete. Check json file.----------')

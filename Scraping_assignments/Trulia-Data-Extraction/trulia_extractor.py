8e3016bd029e230357fc407b096a8d67b65c8560
Abhisek Roy <abhisekroy1994@gmail.com>
2018-09-14 21:32:03 +0530

    Changes in Trulia Extractor

--- Scraping_assignments/Trulia-Data-Extraction/trulia_extractor.py
+++ Scraping_assignments/Trulia-Data-Extraction/trulia_extractor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import urllib.request
 import urllib.parse
 import urllib.error
@@ -9,22 +10,29 @@ import json
 import ast
 import os
 from urllib.request import Request, urlopen
+
 # For ignoring SSL certificate errors
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url=input("Enter Trulia Product Url- ")
+# Input from user
 
+url = input('Enter Trulia Product Url- ')
 
+# Making the website believe that you are accessing it using a mozilla browser
 
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 
+# Creating a BeautifulSoup object of the html page for easy extraction of data.
+
 soup = BeautifulSoup(webpage, 'html.parser')
 html = soup.prettify('utf-8')
 product_json = {}
+
+# This code block will get you a one liner description of the listed property
 
 for meta in soup.findAll('meta', attrs={'name': 'description'}):
     try:
@@ -33,35 +41,49 @@ for meta in soup.findAll('meta', attrs={'name': 'description'}):
     except:
         pass
 
+# This code block will get you the link of the listed property
+
 for link in soup.findAll('link', attrs={'rel': 'canonical'}):
     try:
         product_json['link'] = link['href']
         break
     except:
         pass
-for scripts in soup.findAll('script', attrs={'type': 'application/ld+json'}):
+
+# This code block will get you the price and the currency of the listed property
+
+for scripts in soup.findAll('script',
+                            attrs={'type': 'application/ld+json'}):
     details_json = ast.literal_eval(scripts.text.strip())
-product_json["price"]={}
-product_json["price"]["amount"]=details_json["offers"]["price"]
-product_json["price"]["currency"]=details_json["offers"]["priceCurrency"]
+product_json['price'] = {}
+product_json['price']['amount'] = details_json['offers']['price']
+product_json['price']['currency'] = details_json['offers'
+        ]['priceCurrency']
+
+# This code block will get you the detailed description of the the listed property
 
 for paragraph in soup.findAll('p', attrs={'id': 'propertyDescription'}):
-    product_json["broad-description"] = paragraph.text.strip()
-product_json["overview"]=[]
-for divs in soup.findAll('div', attrs={'data-auto-test-id':'home-details-overview'}):
+    product_json['broad-description'] = paragraph.text.strip()
+product_json['overview'] = []
+
+# This code block will get you the important points regarding the listed property
+
+for divs in soup.findAll('div',
+                         attrs={'data-auto-test-id': 'home-details-overview'
+                         }):
     for divs_second in divs.findAll('div'):
         for uls in divs_second.findAll('ul'):
             for lis in uls.findAll('li', text=True, recursive=False):
-                product_json["overview"].append(lis.text.strip())
+                product_json['overview'].append(lis.text.strip())
 
-
+# Creates a json file with all the information that you extracted
 
 with open('house_details.json', 'w') as outfile:
     json.dump(product_json, outfile, indent=4)
 
+# Creates an html file in your local with the html content of the page you parsed.
 
 with open('output_file.html', 'wb') as file:
     file.write(html)
-
 
 print ('----------Extraction of data is complete. Check json file.----------')

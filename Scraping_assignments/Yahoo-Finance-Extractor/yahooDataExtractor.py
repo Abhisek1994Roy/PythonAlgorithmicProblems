1bdda6de212038fd1124ea80502fc137733fe15f
Abhisek Roy <abhisekroy1994@gmail.com>
2019-05-05 00:54:10 +0530

    changes

--- Scraping_assignments/Yahoo-Finance-Extractor/yahooDataExtractor.py
+++ Scraping_assignments/Yahoo-Finance-Extractor/yahooDataExtractor.py
@@ -19,7 +19,7 @@ ctx.verify_mode = ssl.CERT_NONE
 
 # Input from user
 
-url = input('Enter Trulia Property Listing Url- ')
+url = input('Enter Yahoo Finance Company Url- ')
 
 # Making the website believe that you are accessing it using a mozilla browser
 
@@ -30,91 +30,96 @@ webpage = urlopen(req).read()
 
 soup = BeautifulSoup(webpage, 'html.parser')
 html = soup.prettify('utf-8')
-product_json = {}
-other_details={}
+company_json = {}
+other_details = {}
 
-for span in soup.findAll('span', attrs={'class': 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'}):
-    product_json['PRESENT_VALUE']=span.text.strip()
+for span in soup.findAll('span',
+                         attrs={'class': 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'
+                         }):
+    company_json['PRESENT_VALUE'] = span.text.strip()
 
 for div in soup.findAll('div', attrs={'class': 'D(ib) Va(t)'}):
     for span in div.findAll('span', recursive=False):
-        product_json['PRESENT_GROWTH']=span.text.strip()
+        company_json['PRESENT_GROWTH'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'PREV_CLOSE-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['PREV_CLOSE']=span.text.strip()
+        other_details['PREV_CLOSE'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'OPEN-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['OPEN']=span.text.strip()
+        other_details['OPEN'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'BID-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['BID']=span.text.strip()
+        other_details['BID'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'ASK-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['ASK']=span.text.strip()
+        other_details['ASK'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'DAYS_RANGE-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['DAYS_RANGE']=span.text.strip()
+        other_details['DAYS_RANGE'] = span.text.strip()
 
-for td in soup.findAll('td', attrs={'data-test': 'FIFTY_TWO_WK_RANGE-value'}):
+for td in soup.findAll('td',
+                       attrs={'data-test': 'FIFTY_TWO_WK_RANGE-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['FIFTY_TWO_WK_RANGE']=span.text.strip()
+        other_details['FIFTY_TWO_WK_RANGE'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'TD_VOLUME-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['TD_VOLUME']=span.text.strip()
+        other_details['TD_VOLUME'] = span.text.strip()
 
-for td in soup.findAll('td', attrs={'data-test': 'AVERAGE_VOLUME_3MONTH-value'}):
+for td in soup.findAll('td',
+                       attrs={'data-test': 'AVERAGE_VOLUME_3MONTH-value'
+                       }):
     for span in td.findAll('span', recursive=False):
-        other_details['AVERAGE_VOLUME_3MONTH']=span.text.strip()
+        other_details['AVERAGE_VOLUME_3MONTH'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'MARKET_CAP-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['MARKET_CAP']=span.text.strip()
+        other_details['MARKET_CAP'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'BETA_3Y-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['BETA_3Y']=span.text.strip()
+        other_details['BETA_3Y'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'PE_RATIO-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['PE_RATIO']=span.text.strip()
+        other_details['PE_RATIO'] = span.text.strip()
 
 for td in soup.findAll('td', attrs={'data-test': 'EPS_RATIO-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['EPS_RATIO']=span.text.strip()
+        other_details['EPS_RATIO'] = span.text.strip()
 
-for td in soup.findAll('td', attrs={'data-test': 'EARNINGS_DATE-value'}):
-    other_details['EARNINGS_DATE']=[]
+for td in soup.findAll('td', attrs={'data-test': 'EARNINGS_DATE-value'
+                       }):
+    other_details['EARNINGS_DATE'] = []
     for span in td.findAll('span', recursive=False):
         other_details['EARNINGS_DATE'].append(span.text.strip())
 
-for td in soup.findAll('td', attrs={'data-test': 'DIVIDEND_AND_YIELD-value'}):
-    other_details['DIVIDEND_AND_YIELD']=td.text.strip()
+for td in soup.findAll('td',
+                       attrs={'data-test': 'DIVIDEND_AND_YIELD-value'}):
+    other_details['DIVIDEND_AND_YIELD'] = td.text.strip()
 
-for td in soup.findAll('td', attrs={'data-test': 'EX_DIVIDEND_DATE-value'}):
+for td in soup.findAll('td',
+                       attrs={'data-test': 'EX_DIVIDEND_DATE-value'}):
     for span in td.findAll('span', recursive=False):
-        other_details['EX_DIVIDEND_DATE']=span.text.strip()
+        other_details['EX_DIVIDEND_DATE'] = span.text.strip()
 
-for td in soup.findAll('td', attrs={'data-test': 'ONE_YEAR_TARGET_PRICE-value'}):
+for td in soup.findAll('td',
+                       attrs={'data-test': 'ONE_YEAR_TARGET_PRICE-value'
+                       }):
     for span in td.findAll('span', recursive=False):
-        other_details['NE_YEAR_TARGET_PRICE']=span.text.strip()
+        other_details['ONE_YEAR_TARGET_PRICE'] = span.text.strip()
 
-product_json["OTHER_DETAILS"] = other_details
+company_json['OTHER_DETAILS'] = other_details
 
+with open('data1.json', 'w') as outfile:
+    json.dump(company_json, outfile, indent=4)
 
-
-
-with open('data.json', 'w') as outfile:
-    json.dump(product_json, outfile, indent=4)
-
-print (product_json)
-
-
+print (company_json)
 
 with open('output_file.html', 'wb') as file:
     file.write(html)

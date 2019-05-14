7a8e4ff17c8c4d4d258b4762ec66865b68e0c622
Abhisek Roy <abhisekroy1994@gmail.com>
2019-05-14 21:57:47 +0530

    changes

--- Scraping_assignments/Youtube-Data-Extractor/youtubeVideoPageDataExtractor/youtubeDataExtractor.py
+++ Scraping_assignments/Youtube-Data-Extractor/youtubeVideoPageDataExtractor/youtubeDataExtractor.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import ssl
 import json
 import ast
+import json
 import os
 from urllib.request import Request, urlopen
 
@@ -33,41 +34,35 @@ html = soup.prettify('utf-8')
 company_json = {}
 other_details = {}
 
-# button title="I like this"  - number of likes
-# button title="I dislike this"  - number of dislikes
-# span class="watch-title"  - Title
-# div class="watch-view-count"
-# span class="yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count"
-# <span class="standalone-collection-badge-renderer-text">
-# <a class="yt-uix-sessionlink" data-sessionlink="ei=SRLTXN-9IuW5z7sPkYeDiAI" data-url="/results?search_query=%23GameOfThrones" href="/results?search_query=%23GameOfThrones">
-# #GameOfThrones
-# </a>
-# <a class="yt-uix-sessionlink" data-sessionlink="ei=SRLTXN-9IuW5z7sPkYeDiAI" data-url="/results?search_query=%23RaminDjawadi" href="/results?search_query=%23RaminDjawadi">
-# #RaminDjawadi
-# </a>
-# <a class="yt-uix-sessionlink" data-sessionlink="ei=SRLTXN-9IuW5z7sPkYeDiAI" data-url="/results?search_query=%23TheNightKing" href="/results?search_query=%23TheNightKing">
-# #TheNightKing
-# </a>
-# </span>
+for span in soup.findAll('span',attrs={'class': 'watch-title'}):
+    company_json['TITLE'] = span.text.strip()
 
-# for span in soup.findAll('span',
-#                          attrs={'class': 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'
-#                          }):
-#     company_json['PRESENT_VALUE'] = span.text.strip()
+for script in soup.findAll('script',attrs={'type': 'application/ld+json'}):
+        channelDesctiption = json.loads(script.text.strip())
+        company_json['CHANNEL_NAME'] = channelDesctiption['itemListElement'][0]['item']['name']
 
-# for div in soup.findAll('div', attrs={'class': 'D(ib) Va(t)'}):
-#     for span in div.findAll('span', recursive=False):
-#         company_json['PRESENT_GROWTH'] = span.text.strip()
-#
-# for td in soup.findAll('td', attrs={'data-test': 'PREV_CLOSE-value'}):
-#     for span in td.findAll('span', recursive=False):
-#         other_details['PREV_CLOSE'] = span.text.strip()
-#
-# for td in soup.findAll('td', attrs={'data-test': 'OPEN-value'}):
-#     for span in td.findAll('span', recursive=False):
-#         other_details['OPEN'] = span.text.strip()
+for div in soup.findAll('div',attrs={'class': 'watch-view-count'}):
+    company_json['NUMBER_OF_VIEWS'] = div.text.strip()
+
+for button in soup.findAll('button',attrs={'title': 'I like this'}):
+    company_json['LIKES'] = button.text.strip()
+
+for button in soup.findAll('button',attrs={'title': 'I dislike this'}):
+    company_json['DISLIKES'] = button.text.strip()
+
+for span in soup.findAll('span',attrs={'class': 'yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count'}):
+    company_json['NUMBER_OF_SUBSCRIPTIONS'] = span.text.strip()
+
+hashtags = []
+for span in soup.findAll('span',attrs={'class': 'standalone-collection-badge-renderer-text'}):
+    for a in span.findAll('a',attrs={'class': 'yt-uix-sessionlink'}):
+        hashtags.append(a.text.strip())
+company_json['HASH_TAGS'] = hashtags
 
 with open('output_file.html', 'wb') as file:
     file.write(html)
+
+with open('data.json', 'w', encoding='utf8') as outfile:
+    json.dump(company_json, outfile, ensure_ascii=False,indent=4)
 
 print ('----------Extraction of data is complete. Check json file.----------')
